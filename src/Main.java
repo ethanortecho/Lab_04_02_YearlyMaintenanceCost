02ba170a6ba7a703de11c7e5c33ef432978cbcb3
Ethan Ortecho <ethan.ortecho@gmail.com>
2025-09-15 19:49:13 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        double springCost = 10.2;
+        double summerCost = 12.5;
+        double fallCost = 23.4;
+        double winterCost = 25.1;
+
+        double totalCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("Spring Cost: $" + springCost);
+        System.out.println("Summer Cost: $" + summerCost);
+        System.out.println("Fall Cost: $" + fallCost);
+        System.out.println("Winter Cost: $" + winterCost);
+        System.out.println("Total cost: $" + totalCost);
+
+
+    }
+}
